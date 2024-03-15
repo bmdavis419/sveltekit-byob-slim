c30efdd63e7f1cbaf252fb3d8a53e183b4121210
rykuno <rykuno@gmail.com>
2024-03-15 14:29:31 -0500

    Updated http handlers

--- src/routes/api/[...slugs]/+server.ts
+++ src/routes/api/[...slugs]/+server.ts
@@ -12,3 +12,5 @@ export const DELETE: RequestHandler = ({ request }) => app.fetch(request);
 export const POST: RequestHandler = ({ request }) => app.fetch(request);
 export const PATCH: RequestHandler = ({ request }) => app.fetch(request);
 export const OPTIONS: RequestHandler = ({ request }) => app.fetch(request);
+export const HEAD: RequestHandler = ({ request }) => app.fetch(request);
+export const fallback: RequestHandler = ({ request }) => app.fetch(request);
