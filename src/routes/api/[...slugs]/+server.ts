33fafc483a68920ff1c26b8d3b13b8a005ca7067
rykuno <rykuno@gmail.com>
2024-03-15 14:29:18 -0500

    added missing methods. Updated vite.config.

--- src/routes/api/[...slugs]/+server.ts
+++ src/routes/api/[...slugs]/+server.ts
@@ -10,3 +10,5 @@ export const GET: RequestHandler = ({ request }) => app.fetch(request);
 export const PUT: RequestHandler = ({ request }) => app.fetch(request);
 export const DELETE: RequestHandler = ({ request }) => app.fetch(request);
 export const POST: RequestHandler = ({ request }) => app.fetch(request);
+export const PATCH: RequestHandler = ({ request }) => app.fetch(request);
+export const OPTIONS: RequestHandler = ({ request }) => app.fetch(request);
